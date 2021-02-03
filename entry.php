44203e7465e7cb82b171ba0488ff3bdd5b112a22
andersonvalinhoss <valinhos.anderson@hotmail.com>
2021-02-03 11:10:23 -0300

    atualização estrutura, com html e css da pagina;

--- entry.php
+++ entry.php
@@ -9,7 +9,12 @@ if (!isset($_SESSION["username"]))
 <!DOCTYPE html>
 <html>
 
+
     <head>
+      <style type="text/css">
+      body, div, iframe {margin:0; padding:0; height:100%}
+      iframe {display:block; width:100%; border:none; position:absolute;}
+      </style>
           <title>login</title>
           <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
           <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
@@ -22,9 +27,13 @@ if (!isset($_SESSION["username"]))
       <h3 align="center">Login e registro</h3>
       <br />
     <?php
-    echo '<h1>Bem vindo -'.$_SESSION["username"].'</h1>';
+    //echo '<h1>Bem vindo -'.$_SESSION["username"].'</h1>';
+
+    echo '<a href="sucuri/index.html">Link1</a>';
     echo '<label><a href="logout.php">Logout</a></label>';
+
      ?>
     </div>
+     <iframe src="clone.html" title="W3Schools Free Online Web Tutorials"></iframe>
   </body>
 </html>
